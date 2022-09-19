49d2b29e4cccbb1d72f6394f68cb5709b1e731aa
tanaliga100 <jordantanaliga@gmail.com>
2022-09-19 17:33:13 +0800

    finale-modified

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 const Header = () => {
   return (
     <div>
-    <h1 className='title'>Expense Tracker</h1>
-    <p className='para'>a Simple expense tracker... </p>
-
+      <h1 className="title">Expense Tracker</h1>
+      <small className="para">a simple expense tracker... </small>
     </div>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
